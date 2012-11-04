5e30d31024885eee199a35915a09dd2669c8a590
Yggdrasill <yggdrasill@lavabit.com>
2012-11-04 04:33:58 +0100

    Removed that god-awful camelCasing. Stopped being a faggot.

--- vigrior.c
+++ vigrior.c
@@ -1,10 +1,12 @@
-/* Generates a hash from str, and then divides hash by hashMax, returning the remainder. */
+#include "vigrior.h"
 
-uint32_t genHash(char *str, uint32_t hashMax)
+/* Generates a hash from str, and then divides hash by max, returning the remainder. */
+
+uint32_t gen_hash(char *str, uint32_t max)
 {
   unsigned hash = 0;
   unsigned len = strlen(str);
-  for(int i = 0; i < len; i++ ) {
+  for(unsigned i = 0; i < len; i++ ) {
     hash += str[i];
     hash += ( hash << 10 );
     hash ^= ( hash >> 6 );
@@ -14,16 +16,16 @@ uint32_t genHash(char *str, uint32_t hashMax)
   hash ^= ( hash >> 11 );
   hash += ( hash << 15 );
 
-  return hash % hashMax;
+  return hash % max;
 }
 
 /* Checks for hash collisions. Returns non-zero on collision and zero otherwise. */
 
-int hashColCk(char *s1, char *s2, uint32_t hashMax)
+int check_hash(char *s1, char *s2, uint32_t max)
 {
   int hashCol = 0;
-  uint32_t hash = genHash(s1, hashMax);
-  uint32_t hash2 = genHash(s2, hashMax);
+  uint32_t hash = gen_hash(s1, max);
+  uint32_t hash2 = gen_hash(s2, max);
   if(hash == hash2 && strcmp(s1, s2) )
     hashCol = 1;
   return hashCol;
--- vigrior.h
+++ vigrior.h
@@ -1,7 +1,5 @@
 #include <string.h>
 #include <stdint.h>
 
-uint32_t genHash(char *, uint32_t);
-int hashColCk(char *, char *, uint32_t);
-
-#include "vigrior.c"
+uint32_t gen_hash(char *, uint32_t);
+int check_hash(char *, char *, uint32_t);
